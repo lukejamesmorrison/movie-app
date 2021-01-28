e5da1bf14929b266aeaba6c2ffaebdc7979228cf
lukejamesmorrison <lukejamesmorrison@gmail.com>
2021-01-28 11:03:35 -0500

    Initial commit

---
+++ lib/app.rb
@@ -0,0 +1,17 @@
+require "version"
+
+class App
+
+  def initialize(name)
+    
+  end
+
+
+
+
+
+
+
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/version.rb
@@ -0,0 +1,4 @@
+module App
+  VERSION = "0.1.0"
+end
+
